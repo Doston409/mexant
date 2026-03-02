aa17670930b7c566e99d7a8c9b905b8a47fc9ee1
Пользователь <dabdugapparov806@gmail.com>
2026-03-02 22:59:44 +0500

    nmadur

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'vazifa',
+    'vazifa1',
 ]
 
 MIDDLEWARE = [
---
+++ vazifa1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Vazifa1Config(AppConfig):
+    name = 'vazifa1'
