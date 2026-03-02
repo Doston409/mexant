4bc1132e8d4a25b1d6715bdff78a685f270eee4d
Пользователь <dabdugapparov806@gmail.com>
2026-03-03 01:15:01 +0500

    Fix URLs for our-services and other views

--- vazifa/urls.py
+++ vazifa/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),       
     path('about/', views.about, name='about'),  
     path('contact/', views.contact, name='contact'),  
-    path('our/', views.contact, name='ourcontact'),  
+    path('ourcontact/', views.ourcontact, name='ourcontact'),  
 
 
 ]
